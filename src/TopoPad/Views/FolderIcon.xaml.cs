2b41c9515e00a1000a5db11a9bbd79f88a87755c
Andrew Vardeman <andrewdev@the-vardemans.net>
2020-05-10 17:53:05 -0500

    Added Group and Layer icons (thanks FontAwesome!)

---
+++ src/TopoPad/Views/FolderIcon.xaml.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) 2020 Andrew Vardeman.  Published under the MIT license.
+// See license.txt in the TopoPad distribution or repository for the
+// full text of the license.
+
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace TopoPad.Views
+{
+    public class FolderIcon : UserControl
+    {
+        public FolderIcon()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ src/TopoPad/Views/LayerPlusIcon.xaml.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) 2020 Andrew Vardeman.  Published under the MIT license.
+// See license.txt in the TopoPad distribution or repository for the
+// full text of the license.
+
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace TopoPad.Views
+{
+    public class LayerPlusIcon : UserControl
+    {
+        public LayerPlusIcon()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
